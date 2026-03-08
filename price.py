c2a2d45402ae79551e8df064497aaf6fab209e9d
Eric P <epas001@hotmail.com>
2026-03-08 19:55:10 -0400

    Wrote generic get_from_db function

--- price.py
+++ price.py
@@ -1,32 +1,31 @@
 import duckdb
 import os
 
-def get_from_db():
-    print("Data fetched.")
-#     # --- Configuration ---
-#     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-#     JSON_FILE = os.path.join(SCRIPT_DIR, "Toronto_Island_Ferry_Ticket_Counts.json")
-#     TABLE_NAME = "ferry_ticket_counts"
-#     DB_PATH = os.path.join(SCRIPT_DIR, "my_database.duckdb")
+def get_from_db(in_file,table_name):
+  # --- Configuration ---
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    CSV_FILE_1 = os.path.join(SCRIPT_DIR, in_file)
+    TABLE_NAME = table_name
+    DB_PATH = os.path.join(SCRIPT_DIR, "my_database.duckdb")
 
-#     # --- Connect ---
-#     con = duckdb.connect(DB_PATH)
+    # --- Connect ---
+    con = duckdb.connect(DB_PATH)
 
-#     # --- Read local JSON file into a table ---
-#     con.sql(f"""
-#         CREATE OR REPLACE TABLE {TABLE_NAME} AS
-#         SELECT *
-#         FROM read_json_auto('{JSON_FILE}');
-#     """)
+    # --- Read local CSV file into a table ---
+    con.sql(f"""
+        CREATE OR REPLACE TABLE {TABLE_NAME} AS
+        SELECT *
+        FROM read_csv('{CSV_FILE_1}');
+    """)
 
-#     # --- Query the results ---
-#     print(con.sql(f"SELECT count(*) AS row_count FROM {TABLE_NAME}").fetchone())
-#     print(con.sql(f"SELECT * FROM {TABLE_NAME} LIMIT 5").df())
+    # --- Query the results ---
+    # print(con.sql(f"SELECT count(*) AS row_count FROM {TABLE_NAME}").fetchone())
+    # print(con.sql(f"SELECT * FROM {TABLE_NAME} LIMIT 5").df())
 
-#     con.close()
+    con.close()
 
 def main():
-    get_from_db()
+    get_from_db("price_history.csv","price_history")
 
 if __name__ == "__main__":
     main()
